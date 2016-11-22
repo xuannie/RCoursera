507b9f0d2e0edb9f30dfa905ee2738dfeb2aa037
xuannie <alexiscyx@gmail.com>
2016-11-22 12:02:27 -0600

    11/22 Working correlation

--- Week2/corr.R
+++ Week2/corr.R
@@ -1,22 +1,29 @@
 corr  <- function(directory, threshold = 0) {
       ## Import files into list, retain full directory path
       files <- list.files(directory, full.names=TRUE)
-  
       ## Create data frame
       dat <- data.frame()
-  
-      ## For loop reads csv file and adds to data frame
-      for (i in 1:332){
-        dat <- rbind(dat, read.csv(files[i]))
-      }
-  
+
       ## Get Number of observations for each case
       cnobs <- complete(directory)
-      ## Get NOBS that fulfill threshold
+      
+      ## Get ids that fulfill threshold
       ids   <- cnobs[cnobs$nobs > threshold, ]$id
-      cr    <- data.frame()
+      
+      ##create data frame, numeric vector to return
+      crdat <- data.frame()
+      cr <- numeric()
+      
       for (x in ids){
-          cr <- rbind(cr, dat[dat$ID == x, ])
+          ##read file that fulfills threshold
+          crdat <- read.csv(files[x])
+          
+          ##get only complete cases
+          valid_crdat <- crdat[complete.cases(crdat), ]
+          
+          ##calculate correlation
+          cr = c(cr, cor(valid_crdat$sulfate, valid_crdat$nitrate))
       }
-      cr      
+      
+      cr
 }
